e68fd2662ff4a708af15823aa1ff3c422e0e27d8
matieu-d <mathieu.dupriez@gmail.com>
2026-03-29 17:44:10 +0200

    Add Hailo-10H support

--- docker/main/install_hailort.sh
+++ docker/main/install_hailort.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-hailo_version="4.21.0"
+hailo_version="5.1.1"
 
 if [[ "${TARGETARCH}" == "amd64" ]]; then
     arch="x86_64"
@@ -10,5 +10,5 @@ elif [[ "${TARGETARCH}" == "arm64" ]]; then
     arch="aarch64"
 fi
 
-wget -qO- "https://github.com/frigate-nvr/hailort/releases/download/v${hailo_version}/hailort-debian12-${TARGETARCH}.tar.gz" | tar -C / -xzf -
-wget -P /wheels/ "https://github.com/frigate-nvr/hailort/releases/download/v${hailo_version}/hailort-${hailo_version}-cp311-cp311-linux_${arch}.whl"
+wget -qO- "https://github.com/mathieu-d/hailort/releases/download/v${hailo_version}/hailort-debian12-${TARGETARCH}.tar.gz" | tar -C / -xzf -
+wget -P /wheels/ "https://github.com/mathieu-d/hailort/releases/download/v${hailo_version}/hailort-${hailo_version}-cp311-cp311-linux_${arch}.whl"
